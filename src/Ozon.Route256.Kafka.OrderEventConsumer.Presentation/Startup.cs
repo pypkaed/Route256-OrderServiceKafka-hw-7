afdb4230479ca39baff09af14554a5c61730413a
Orlov Dmitriy Aleksandrovich <dmorlov@ozon.ru>
2023-10-19 18:35:53 +0300

    Add homework

---
+++ src/Ozon.Route256.Kafka.OrderEventConsumer.Presentation/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Ozon.Route256.Kafka.OrderEventConsumer.Domain;
+using Ozon.Route256.Kafka.OrderEventConsumer.Infrastructure;
+using Ozon.Route256.Kafka.OrderEventConsumer.Infrastructure.Common;
+
+namespace Ozon.Route256.Kafka.OrderEventConsumer.Presentation;
+
+public sealed class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration) => _configuration = configuration;
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        var connectionString = _configuration["ConnectionString"]!;
+
+        services
+            .AddFluentMigrator(
+                connectionString,
+                typeof(SqlMigration).Assembly);
+
+        services.AddScoped<IItemRepository, ItemRepository>(_ => new ItemRepository(connectionString));
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+    }
+}
